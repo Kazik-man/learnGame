ebbaaf3c197432e66f954725b866d01871c1c93e
Andrii Boyko <boyko.a@nimses.com>
2019-04-26 15:39:00 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,145 +1,181 @@
 const objectMas = [
-  {
-    "image": "./img/kiev.jpg",
-    "name": "Kiev",
-    "count": "400"
-  },
-  {
-    "image": "./img/china.jpg",
-    "name": "China",
-    "count": "500"
-  },
-  {
-    "image": "./img/france.jpg",
-    "name": "France",
-    "count": "600"
-  },
-  {
-    "image": "./img/georgia.jpg",
-    "name": "Georgia",
-    "count": "700"
-  },
-  {
-    "image": "./img/ny.jpg",
-    "name": "America",
-    "count": "500"
-  },
-  {
-    "image": "./img/praga.jpg",
-    "name": "Prague",
-    "count": "400"
-  },
-  {
-    "image": "./img/Rome.jpg",
-    "name": "Rome",
-    "count": "400"
-  },
-  {
-    "image": "./img/russia.jpg",
-    "name": "Russia",
-    "count": "600"
-  },
-  {
-    "image": "./img/Stambule.jpg",
-    "name": "Stambule",
-    "count": "700"
-  },
-  ]
+    {
+        "image": "./img/kiev.jpg",
+        "name": "Kiev",
+        "count": "400"
+    },
+    {
+        "image": "./img/china.jpg",
+        "name": "China",
+        "count": "500"
+    },
+    {
+        "image": "./img/france.jpg",
+        "name": "France",
+        "count": "600"
+    },
+    {
+        "image": "./img/georgia.jpg",
+        "name": "Georgia",
+        "count": "700"
+    },
+    {
+        "image": "./img/ny.jpg",
+        "name": "America",
+        "count": "500"
+    },
+    {
+        "image": "./img/praga.jpg",
+        "name": "Prague",
+        "count": "850"
+    },
+    {
+        "image": "./img/Rome.jpg",
+        "name": "Rome",
+        "count": "350"
+    },
+    {
+        "image": "./img/russia.jpg",
+        "name": "Russia",
+        "count": "600"
+    },
+    {
+        "image": "./img/Stambule.jpg",
+        "name": "Stambule",
+        "count": "700"
+    },
+]
 
-let countScore = 0;
+let COUNT_SCORE = 0;
 let COUNT_LEVEL = 500;
 let COUNT_BOX = 100;
 
-const containerWords =document.getElementById('words');
+const containerWords = document.getElementById('words');
 const bgZona = $('#BG_game');
 
 let corentGameState;
 let hz = [];
 
 function createNewLevel() {
-	$('.miniBox').show();
-// берет случайный элемент из массива городов
-	const randomIndex = getRandomArbitrary(0, objectMas.length - 1);
+    $('.miniBox').show();
+    // берет случайный элемент из массива городов
+    const randomIndex = getRandomArbitrary(0, objectMas.length - 1);
 
     // случайное число от 1 до 9
-  const randomEmptyBox = getRandomArbitrary(1, 9);
-  console.log(randomIndex, objectMas[randomIndex]);
-  console.log('randomEmptyBox',randomEmptyBox);
+    const randomEmptyBox = getRandomArbitrary(1, 9);
+    console.log(randomIndex, objectMas[randomIndex]);
+    console.log('randomEmptyBox', randomEmptyBox);
 
-  // обьект случайного города
-  corentGameState = objectMas[randomIndex];
-  bgZona.css("background-image", `url(${corentGameState.image})`);
+    // обьект случайного города
+    corentGameState = objectMas[randomIndex];
+    bgZona.css("background-image", `url(${corentGameState.image})`);
+    COUNT_LEVEL = corentGameState.count;
 
-  //  удаляет случайно выбранный элемент
-  deleteBox(randomEmptyBox);
-
-		// create list name
-  loadWords(corentGameState.name);
-  hz = []
+    //  удаляет случайно выбранный элемент
+    deleteBox(randomEmptyBox);
+    hz = [];
+    // create list name
+    loadWords(corentGameState.name);
 }
 
 
 function initGame() {
-	createNewLevel();
-	updateStatus(COUNT_LEVEL, COUNT_BOX);
-	nextLevel();
-	// keyCode();
+    createNewLevel();
+    updateStatus();
 }
 
 function getRandomArbitrary(min, max) {
- return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 initGame();
 
-$(document).on('keyup', function(event) {
-	console.log(event.key)
-   // "france".includes("w") => boolean
-	if (corentGameState.name.includes(event.key)) {
-		// open letter
+$(document).on('keyup', function (event) {
+    const currentName = corentGameState.name.toLowerCase(),
+        currentLetter = event.key.toLowerCase();
+    console.log(currentName, currentLetter);
+// "france".includes("w") => boolean
+    if (currentName.includes(currentLetter)) {
+// open letter
 
-		// ["u", "a"]
-		// hz.push(event.code)
-		// ["u", "a", "i"]
+// ["u", "a"]
+// hz.push(event.code)
+// ["u", "a", "i"]
 
 
-        hz.push(event.key)
+        hz.push(event.key);
 
-	    loadWords(corentGameState.name);
+        loadWords(corentGameState.name);
+    } else {
+// reduce score
+        COUNT_LEVEL -= 50;
+        if (COUNT_LEVEL < 0) {
+            COUNT_LEVEL = 0;
+            alert('You are loser!!!!');
+            return;
+        }
+        updateStatus(COUNT_LEVEL, COUNT_BOX);
+    }
+});
 
-	} else {
-		// reduce score
-		COUNT_LEVEL -= 50
-		updateStatus(COUNT_LEVEL, COUNT_BOX);
-	}
+// word: france
+// hz: nce
 
-})
+function loadWords(name) {
+    $(containerWords).empty();
+    console.log(name.split(""));
+    name.split("").forEach(function (letter, index) {
+        console.log('letter', letter, 'index', index);
+        const el = document.createElement("div");
+        el.className = 'words_item';
+        console.log('hz', hz, hz.includes(letter.toLowerCase()));
+        if (!hz.includes(letter.toLowerCase())) {
+            el.innerHTML = index + 1
+        } else {
+            el.innerHTML = letter
+        }
 
-$('.miniBox').click(function(e) {
-	const can = count();
-	if ( can ) {
-		$(this).hide();
-	}
-	updateStatus(COUNT_LEVEL, COUNT_BOX);
+        containerWords.appendChild(el);
+    });
+    checkWord();
+}
+
+function checkWord() {
+    console.log(corentGameState.name.length, hz.length);
+    if (corentGameState.name.length === hz.length) {
+        alert('you are win :))))');
+        COUNT_SCORE = Number(COUNT_SCORE) + Number(COUNT_LEVEL);
+        createNewLevel();
+        updateStatus();
+    }
+}
+
+
+$('.miniBox').click(function (e) {
+    const can = count();
+    if (can) {
+        $(this).hide();
+    }
+    updateStatus(COUNT_LEVEL, COUNT_BOX);
 });
 
 function count() {
-	console.log(COUNT_LEVEL, COUNT_BOX);
-	if (COUNT_LEVEL >= COUNT_BOX) {
-		COUNT_LEVEL = COUNT_LEVEL - COUNT_BOX;
-		COUNT_BOX = COUNT_BOX + 100;
-		console.log('score_level', COUNT_BOX);
-		return true;
+    console.log(COUNT_LEVEL, COUNT_BOX);
+    if (COUNT_LEVEL >= COUNT_BOX) {
+        COUNT_LEVEL = COUNT_LEVEL - COUNT_BOX;
+        COUNT_BOX = COUNT_BOX + 100;
+        console.log('score_level', COUNT_BOX);
+        return true;
     }
-	return false;
+    return false;
 }
 
 function buttonClick() {
-	initGame();
+    initGame();
 }
 
 function deleteBox(box) {
-	$('.miniBox').eq(box).hide();
+    $('.miniBox').eq(box).hide();
 }
 
 // две функции идентичны.
@@ -154,99 +190,9 @@ function deleteBox(box) {
 // ПАРАМЕТРЫ ФУНКЦИИ!!!!!! ПОВТОРИТЬ!
 
 
-function updateStatus(COUNT_LEVEL, COUNT_BOX) {
-	$('#score').text(countScore);
+function updateStatus() {
+    $('#score').text(COUNT_SCORE);
     $('#score_level').text(COUNT_LEVEL);
     $('#cost_open_box').text(COUNT_BOX);
 }
 
-// word: france
-// hz: nce
-
-function loadWords(aaaaa) {
-	$(containerWords).empty();
-
-    aaaaa.split("").forEach(function (letter, index) {
-		const el = document.createElement("div");
-		el.className = 'words_item';
-
-		if(!hz.includes(letter.toLowerCase())) {
-			el.innerHTML = index + 1
-		} else {
-			el.innerHTML = letter
-		}
-
-		containerWords.appendChild(el)
-	})
-}
-
-function test() {
-    console.log('hello');
-}
-
-
-
-function nextLevel() {
-	const letters = [corentGameState.name.split("")];
-	console.log(letters);
-}
-
-
-function keyCode() {
-	const letterInWord = letters;
-	if( letterInWord.includes('a')) {
-		return true;
-	}
-}
-
-
-
-
-
-// function functionDel() {
-
-// 	// удалить все внутри #words
-
-// 	// в цикле внутри #words  создлать новые дивы
-// 	// количество дивов приходит в функцию как параметр
-// }
-
-// function fun() {
-// 	let words = getRandomArbitrary();
-// 	let wordRandr = words;
-
-// 	let answerArray = [];
-// 	for( let i = 0; i < wordRandr.length; i++ ) {
-// 		answerArray[i] = "_";
-// 	}
-
-// 	let remainingLetters = wordRandr.length;
-// 	alert(answerArray.join(" "));
-// }
-
-
-// over(10,2);
-
-
-// over(2,34);
-
-// function over(count, max) {
-// 	return count > max;
-// }
-
-
-// test({
-// 	sex: 'man',
-// 	age: '30',
-// 	childeren: '1'
-// });
-
-// function test(data) {
-// 	return data.age + data.childeren;
-// }
-
-// strange(['alex','andrew','yulia','olesya','stas','bogdan'], 4);
-
-// function strange(data, i) {
-// 	return data[i - 1]
-// }
